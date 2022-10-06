9d0c5cb300071b4ced0b27e4c59a1f81e535e05a
Rafael <rafael.souza2@alunos.sc.senac.br>
2022-10-06 20:58:50 -0300

    Aula - 06/10

---
+++ src/components/FetchNews.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+/*
+    {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+*/
+const FetchNews = () => {
+    const [news,setNews] = useState([])
+
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+
+    useEffect(() => {
+        fetch(url)
+        //retornar um valor
+        .then((response) => response.json())
+        //retorna 
+        .then((data)=>{
+            setNews(data);
+            console.log(news);
+        });
+    },[]);
+
+    return (
+        <div>
+            <h1>Notícias</h1>
+        </div>
+    )
+}
+
+export default FetchNews;
